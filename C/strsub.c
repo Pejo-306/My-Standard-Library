d886f52ae7a1b1c2612177766123405d0dbc2173
Petar Nikolov <penikolov23@gmail.com>
2018-03-05 12:34:12 +0200

    Improve strsub function

--- C/strsub.c
+++ C/strsub.c
@@ -1,35 +1,38 @@
 #define REPLACES 4
 
-void strsub(char *str, char *search, char *replace)
+int strsub(char *str, char *search, char *replace)
 {
     char *buffer = (char *)malloc(sizeof(char) *
-        (strlen(str)+1 + REPLACES * strlen(replace)));
-    strcpy(buffer, "");
-    int replaces = 0;
-    int offset = 0;
-    for (int i = 0, j; i < strlen(str); ++i) {
-        for (j = 0; j < strlen(search) && str[i+j] == search[j]; ++j);
-        
-        if (j == strlen(search)) { // match found
-            for (j = 0; j < strlen(replace); ++j)
-                buffer[offset+i+j] = replace[j];
-            offset += strlen(replace)-1;
-            replaces++;
-        } else {
-            buffer[offset+i] = str[i];
-        }
+        (strlen(str)+1 + REPLACES * (strlen(replace)-1)));
+    int substitutions = 0;
+    int buffer_i = 0;
+    for (int str_i = 0, j; str_i < strlen(str); str_i++) {
+        for (j = 0; j < strlen(search) && str[str_i+j] == search[j]; ++j);
 
-        if (replaces == REPLACES) {
-            // allocate more memory
-            buffer = (char *)realloc(buffer, sizeof(char) *
-                (strlen(str)+strlen(buffer)+1 + REPLACES * strlen(replace)));
-            replaces = 0;
+        if (j == strlen(search)) { // match found
+            for (int c = 0; c < strlen(replace); ++c)
+                buffer[buffer_i+c] = replace[c];
+            buffer_i += strlen(replace);
+            str_i += strlen(search) - 1;
+            substitutions++;
+
+            if (substitutions % REPLACES == 0)
+                // allocate more memory
+                buffer = (char *)realloc(buffer, sizeof(char) *
+                    (strlen(str)+1 + strlen(buffer)+1 + REPLACES * (strlen(replace)-1)));
+        } else {
+            buffer[buffer_i] = str[str_i];
+            buffer_i++;
         }
     }
-    buffer[offset + strlen(str)] = '\0';
+    buffer[buffer_i] = '\0';
 
-    str = (char *)realloc(str, sizeof(char) * (strlen(buffer)+1));
+    if (substitutions > REPLACES)
+        // additional memory has been allocated
+        str = (char *)realloc(str, sizeof(char) * strlen(buffer)+1);
     strcpy(str, buffer);
     free(buffer);
+
+    return substitutions;
 }
 
