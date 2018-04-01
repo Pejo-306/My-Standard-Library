01cf8dc4240c86011d848f938a72955f817d7b38
Petar Nikolov <penikolov23@gmail.com>
2018-04-01 19:52:00 +0300

    [C] Allow getword to return newline character ('\n')
    Now, newline must explicitly be ignored by the function caller if they
    do not want to utilize it. However, this change can be used in various
    applications such as counting the line number.

--- C/io/getword.c
+++ C/io/getword.c
@@ -5,15 +5,18 @@ int getword(char *word, int limit, int (*char_comp)(int))
     int c;
     char *w = word;
 
-    while (isspace(c = getchar()));
+    while ((c = getchar()) == ' ' || c == '\t');
+    if (c == '\n')
+        return '\n';
     ungetc(c, stdin);
-    while (!(*char_comp)(c = getchar()) && c != EOF);
+    while (!(*char_comp)(c = getchar()) && c != EOF && c != '\n');
     if (c != EOF)
         *w++ = c;
     if (!(*char_comp)(c)) {
         *w = '\0';
         return c;
     }
+
     for ( ; --limit > 0; w++) {
         if (!(*char_comp)(*w = getchar())) {
             ungetc(*w, stdin);
